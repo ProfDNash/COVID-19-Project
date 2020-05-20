33edb5730eb6411cbd5f20e94a29ac25b13887b7
David A. Nash <47865752+ProfDNash@users.noreply.github.com>
2020-05-20 10:34:48 -0400

    Update DataCleaner.py
    Added basic deletion of unwanted columns

--- DataCleaner.py
+++ DataCleaner.py
@@ -5,3 +5,18 @@ Created on Wed May 20 10:08:45 2020
 """
 import numpy as np
 import pandas as pd
+
+filename = r'C:\Users\ProfN\Downloads\daily (1).csv'
+data = pd.read_csv(filename)
+##delete unused columns
+unused = ['pending','hospitalizedCurrently','hospitalizedCumulative','inIcuCurrently','inIcuCumulative',
+          'onVentilatorCurrently','onVentilatorCumulative','dataQualityGrade','hash','dateChecked',
+          'hospitalized','total','posNeg','fips']
+for col in unused:
+    del data[col]
+
+##write edited table to file for use in Tableau
+outputname = r'C:\Users\ProfN\Downloads\daily.csv'
+data.to_csv(outputname)
+
+
